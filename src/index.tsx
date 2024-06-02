f514ae0398b287e3765b91d864a88d42f637ea2b
xa11der <alexander.sijan@gmail.com>
2024-06-02 18:44:21 +0200

    add lazy imports for pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
-import { Collection } from './pages/Collection';
-import { CreateCard } from './pages/CreateCard';
+import { Toaster } from 'react-hot-toast';
 import Header from './components/Header';
+import Notification from './components/ui/Notification';
 import './main.css';
 
+const Collection = lazy(() => import('./pages/Collection'));
+const CreateCard = lazy(() => import('./pages/CreateCard'));
+const EditCard = lazy(() => import('./pages/EditCard'));
+
 const App = () => (
-  <Router>
-    <Header />
-    <Switch>
-      <Route exact path="/collection" component={Collection} />
-      <Route exact path="/create-card" component={CreateCard} />
-    </Switch>
-  </Router>
+  <Suspense fallback={<Notification message="Loading..." />}>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/collection" component={Collection} />
+        <Route exact path="/create-card" component={CreateCard} />
+        <Route exact path="/edit-card/:id" component={EditCard} />
+      </Switch>
+      <Toaster />
+    </Router>
+  </Suspense>
 );
 
 render(<App />, document.getElementById('root'));
